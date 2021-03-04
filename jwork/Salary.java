2539ea4a3feef7c6fa5f3be2b3376bc25760d16e
ariefsaferman <arief.saferman@ui.ac.id>
2021-03-04 17:09:47 +0700

    modul 1

---
+++ jwork/Salary.java
@@ -0,0 +1,47 @@
+
+/**
+ * Write a description of class Salary here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Salary
+{
+    // instance variables - replace the example below with your own
+    private int idCrew, idBuilding, totalSalary; 
+    private String namePerson, joinDate; 
+    private Boolean is5Year;
+
+   
+    public int getIdCrew()
+    {
+        return 0;
+    }
+
+    public int getIdBuilding()
+    {
+        return 0;
+    }
+    
+    public int getTotalSalary()
+    {
+        return 0;
+    }
+    
+    public String getNamePerson( String nama)
+    {
+        nama = "oop";
+        return nama;
+    }
+    
+    public String getJoinDate(String joinDate)
+    {
+        return joinDate;
+    }
+    
+    public Boolean getIs5Year()
+    {
+        return false;
+    }
+    
+}
